7ddb6cea0616968b176656c56e6b6bf20f4413c3
Evgenii Lezhnin <z_lezhnin@mail2000.ru>
2013-10-08 00:53:40 +0700

    inverse

---
+++ inverse.cpp
@@ -0,0 +1,28 @@
+#include "itern.h"
+#include "gauss.h"
+#include <iostream>
+using namespace std;
+void inversed(int t, double **a, int n, double **result, double eps, double &epsf, double *epsv) {
+    double *e = new double[n];
+    double *temp = new double[n];
+    for (int i=0; i<n; i++)
+        e[i] = 0;
+    for (int i=0; i<n; i++) {
+        if (i!=0)
+            e[i-1] = 0;
+        e[i] = 1;
+        if (t==1)
+            gauss(a,n,e,temp,eps,epsf,epsv);
+        else
+            itern(a,n,e,temp,eps,epsf,epsv);
+        for (int j=0; j<n; j++)
+            result[j][i] = temp[j];
+        cout << "\nepsilon vector: ";
+        for(int i=0;i<n;i++)
+            cout << epsv[i] << ' ';
+        cout << "\neps : " << epsf << endl;
+    }
+    delete[] temp;
+    delete[] e;
+}
+
---
+++ inverse.h
@@ -0,0 +1,4 @@
+#ifndef INVERSED_H_
+#define INVERSED_H_
+void inversed(int t, double **a, int n, double **result, double eps, double &epsf, double *epsv);
+#endif
